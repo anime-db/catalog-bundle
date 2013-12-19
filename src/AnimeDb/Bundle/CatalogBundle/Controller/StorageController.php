bbb21d30a08ec81050aee1a5fafa321f8499ea71
Peter Gribanov <info@peter-gribanov.ru>
2013-12-19 16:53:44 +0400

    add parametrs for require item path

--- src/AnimeDb/Bundle/CatalogBundle/Controller/StorageController.php
+++ src/AnimeDb/Bundle/CatalogBundle/Controller/StorageController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AnimeDb\Bundle\CatalogBundle\Entity\Storage;
 use Symfony\Component\HttpFoundation\Request;
 use AnimeDb\Bundle\CatalogBundle\Form\Entity\Storage as StorageForm;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Storages
@@ -108,5 +109,20 @@ class StorageController extends Controller
         $em->remove($storage);
         $em->flush();
         return $this->redirect($this->generateUrl('storage_list'));
+    }
+
+    /**
+     * Get storage path
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Storage $storage
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getPathAction(Storage $storage)
+    {
+        return new JsonResponse([
+            'required' => $storage->isPathRequired(),
+            'path' => $storage->getPath()
+        ]);
     }
 }
