78a456e1697a5ecf73b50bc33f8f416bd8048ae6
Peter Gribanov <info@peter-gribanov.ru>
2014-04-09 12:41:26 +0400

    add widget entitys

---
+++ src/AnimeDb/Bundle/CatalogBundle/Entity/Widget/Item.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Entity\Widget;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use AnimeDb\Bundle\CatalogBundle\Entity\Item as ItemEntity;
+
+/**
+ * Widget item
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Entity\Widget
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Item
+{
+    /**
+     * Cover
+     *
+     * @var string
+     */
+    protected $cover;
+
+    /**
+     * Name
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Link to a item in an external catalog
+     *
+     * @var string
+     */
+    protected $link;
+
+    /**
+     * Item in local catalog if have
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Item|null
+     */
+    protected $item;
+
+    /**
+     * Type
+     *
+     * @var \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Type
+     */
+    protected $type;
+
+    /**
+     * Genre list
+     *
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     */
+    protected $genres;
+
+    /**
+     * Construct
+     */
+    public function __construct() {
+        $this->genres  = new ArrayCollection();
+    }
+
+    /**
+     * Set cover
+     *
+     * @param string $cover
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function setCover($cover)
+    {
+        $this->cover = $cover;
+        return $this;
+    }
+
+    /**
+     * Get cover
+     *
+     * @return string
+     */
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set item
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Item|null $item
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function setItem(ItemEntity $item = null)
+    {
+        $this->item = $item;
+        return $this;
+    }
+
+    /**
+     * Get item
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Item|null
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set type
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Type $type
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function setType(Type $type)
+    {
+        if ($this->type !== $type) {
+            $this->type = $type->setItem($this);
+        }
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Add genres
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Genre $genre
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Widget\Item
+     */
+    public function addGenre(Genre $genre)
+    {
+        if (!$this->genres->contains($genre)) {
+            $this->genres->add($genre);
+            $genre->setItem($this);
+        }
+        return $this;
+    }
+
+    /**
+     * Get genres
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getGenres()
+    {
+        return $this->genres;
+    }
+}
