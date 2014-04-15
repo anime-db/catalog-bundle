4e530e8e16cfdd623aea57b94fdb6c1b27ad6c6f
Peter Gribanov <info@peter-gribanov.ru>
2014-04-15 17:01:27 +0400

    create entity label

---
+++ src/AnimeDb/Bundle/CatalogBundle/Entity/Label.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Label
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="label")
+ * @IgnoreAnnotation("ORM")
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Entity
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Label
+{
+    /**
+     * Id
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     *
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * Label name
+     *
+     * @ORM\Column(type="string", length=16)
+     * @Assert\NotBlank()
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Items list
+     *
+     * @ORM\ManyToMany(targetEntity="Item", mappedBy="labels")
+     *
+     * @var \Doctrine\Common\Collections\ArrayCollection
+     */
+    protected $items;
+
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Label
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Add items
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Item $item
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Label
+     */
+    public function addItem(Item $item)
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->addGenre($this);
+        }
+        return $this;
+    }
+
+    /**
+     * Remove items
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Item $item
+     *
+     * @return \AnimeDb\Bundle\CatalogBundle\Entity\Label
+     */
+    public function removeItem(Item $item)
+    {
+        if ($this->items->contains($item)) {
+            $this->items->removeElement($item);
+            $item->removeGenre($this);
+        }
+        return $this;
+    }
+
+    /**
+     * Get items
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
