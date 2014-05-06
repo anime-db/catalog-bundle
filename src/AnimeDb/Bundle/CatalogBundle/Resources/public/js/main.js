813718ded7ce1109378e43403f51757f945a0a73
Peter Gribanov <info@peter-gribanov.ru>
2014-05-06 13:10:49 +0400

    change timeout

--- src/AnimeDb/Bundle/CatalogBundle/Resources/public/js/main.js
+++ src/AnimeDb/Bundle/CatalogBundle/Resources/public/js/main.js
@@ -163,7 +163,7 @@ $('.list-items .item').each(function() {
 			var that = this;
 			this.timer = setTimeout(function() {
 				that.setKeep();
-			}, 800);
+			}, 1000);
 		},
 		stopTimer: function() {
 			clearTimeout(this.timer);
