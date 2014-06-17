ad184800dc51b7f83d70faabca8c54b34f7abf79
Peter Gribanov <info@peter-gribanov.ru>
2014-06-17 17:52:00 +0400

    show read more link in refill form

--- src/AnimeDb/Bundle/CatalogBundle/Controller/RefillController.php
+++ src/AnimeDb/Bundle/CatalogBundle/Controller/RefillController.php
@@ -89,6 +89,7 @@ class RefillController extends Controller
                     'name' => $search_item->getName(),
                     'image' => $search_item->getImage(),
                     'description' => $search_item->getDescription(),
+                    'source' => $search_item->getSource(),
                     'link' => $this->generateUrl('refiller_search_fill', [
                         'plugin' => $plugin,
                         'field' => $field,
