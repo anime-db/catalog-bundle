8603e2960b8408517de50773d0ba5e33505a5aba
Peter Gribanov <info@peter-gribanov.ru>
2014-06-18 13:02:26 +0400

    add console output export decorator

---
+++ src/AnimeDb/Bundle/CatalogBundle/Console/Output/Decorator.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Console\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Decorator for output
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Console\Output
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Decorator implements OutputInterface
+{
+    /**
+     * Output
+     *
+     * @var \Symfony\Component\Console\Output\OutputInterface
+     */
+    protected $output;
+
+    /**
+     * Construct
+     *
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     */
+    public function __construct(OutputInterface $output)
+    {
+        $output->setDecorated(true);
+        $this->output = $output;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::write()
+     */
+    public function write($messages, $newline = false, $type = self::OUTPUT_NORMAL)
+    {
+        $this->output->write($messages, $newline, $type);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::writeln()
+     */
+    public function writeln($messages, $type = self::OUTPUT_NORMAL)
+    {
+        $this->output->writeln($messages, $type);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::setVerbosity()
+     */
+    public function setVerbosity($level)
+    {
+        $this->output->setVerbosity($level);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::getVerbosity()
+     */
+    public function getVerbosity()
+    {
+        return $this->output->getVerbosity();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::setDecorated()
+     */
+    public function setDecorated($decorated)
+    {
+        $this->output->setDecorated($decorated);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::isDecorated()
+     */
+    public function isDecorated()
+    {
+        return $this->output->isDecorated();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::setFormatter()
+     */
+    public function setFormatter(OutputFormatterInterface $formatter)
+    {
+        $this->output->setFormatter($formatter);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::getFormatter()
+     */
+    public function getFormatter()
+    {
+        return $this->output->getFormatter();
+    }
+}
---
+++ src/AnimeDb/Bundle/CatalogBundle/Console/Output/Export.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Console\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use AnimeDb\Bundle\CatalogBundle\Console\Output\Decorator;
+
+/**
+ * Export output to file
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Console\Output
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Export extends Decorator
+{
+    /**
+     * Filename
+     *
+     * @var string
+     */
+    protected $filename;
+
+    /**
+     * Flags
+     *
+     * @var integer
+     */
+    protected $flags;
+
+    /**
+     * Construct
+     *
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param string $filename
+     * @param integer $flags
+     */
+    public function __construct(OutputInterface $output, $filename, $flags = 0)
+    {
+        parent::__construct($output);
+        $this->filename = $filename;
+        $this->flags = $flags;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::write()
+     */
+    public function write($messages, $newline = false, $type = self::OUTPUT_NORMAL)
+    {
+        file_put_contents($this->filename, implode($newline ? "\n" : '', (array)$messages), $this->flags);
+        parent::write($messages, $newline, $type);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Console\Output\OutputInterface::writeln()
+     */
+    public function writeln($messages, $type = self::OUTPUT_NORMAL)
+    {
+        file_put_contents($this->filename, implode('', (array)$messages), $this->flags);
+        parent::writeln($messages, $type);
+    }
+}
