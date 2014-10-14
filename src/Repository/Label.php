245c1af2f575421fbaa550de08a39836f86528f5
Peter Gribanov <info@peter-gribanov.ru>
2014-10-14 13:14:48 +0400

    add Label repository

---
+++ src/Repository/Label.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Label repository
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Repository
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Label extends EntityRepository
+{
+    
+}
