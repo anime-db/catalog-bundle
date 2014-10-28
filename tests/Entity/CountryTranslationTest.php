7d1de9bab0d1966d7ae77cf0da3158aeba25f2ea
Peter Gribanov <info@peter-gribanov.ru>
2014-10-28 17:27:43 +0300

    add test for country translation

---
+++ tests/Entity/CountryTranslationTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Tests\Entity;
+
+use AnimeDb\Bundle\CatalogBundle\Entity\CountryTranslation;
+
+/**
+ * Test country translation
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Tests\Entity
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class CountryTranslationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test
+     */
+    public function test()
+    {
+        $trans = new CountryTranslation('my_locale', 'my_field', 'my_value');
+        $this->assertEquals('my_locale', $trans->getLocale());
+        $this->assertEquals('my_field', $trans->getField());
+        $this->assertEquals('my_value', $trans->getContent());
+    }
+}
