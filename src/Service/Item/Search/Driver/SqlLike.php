0dcdb0e7f68ea1b684bf0f1201336bc442c47330
Peter Gribanov <info@peter-gribanov.ru>
2014-11-27 18:32:34 +0300

    add selector for build query for search items

--- src/Service/Item/Search/Driver/SqlLike.php
+++ src/Service/Item/Search/Driver/SqlLike.php
@@ -13,10 +13,7 @@ namespace AnimeDb\Bundle\CatalogBundle\Service\Item\Search\Driver;
 use AnimeDb\Bundle\CatalogBundle\Entity\Search;
 use AnimeDb\Bundle\CatalogBundle\Service\Item\Search\DriverInterface;
 use Doctrine\Bundle\DoctrineBundle\Registry;
-use AnimeDb\Bundle\CatalogBundle\Entity\Type as TypeEntity;
-use AnimeDb\Bundle\CatalogBundle\Entity\Country as CountryEntity;
-use AnimeDb\Bundle\CatalogBundle\Entity\Storage as StorageEntity;
-use AnimeDb\Bundle\CatalogBundle\Entity\Studio as StudioEntity;
+use AnimeDb\Bundle\CatalogBundle\Service\Item\Search\Selector;
 
 /**
  * Search driver use a SQL LIKE for select name
@@ -34,13 +31,22 @@ class SqlLike implements DriverInterface
     protected $repository;
 
     /**
+     * Selector
+     *
+     * @var \AnimeDb\Bundle\CatalogBundle\Service\Item\Search\Selector
+     */
+    protected $selector;
+
+    /**
      * Construct
      *
      * @param \Doctrine\Bundle\DoctrineBundle\Registry $doctrine
+     * @param \AnimeDb\Bundle\CatalogBundle\Service\Item\Search\Selector $selector
      */
-    public function __construct(Registry $doctrine)
+    public function __construct(Registry $doctrine, Selector $selector)
     {
         $this->repository = $doctrine->getRepository('AnimeDbCatalogBundle:Item');
+        $this->selector = $selector;
 
         // register custom lower()
         $conn = $doctrine->getConnection()->getWrappedConnection();
@@ -54,7 +60,7 @@ class SqlLike implements DriverInterface
     /**
      * Search items
      * 
-     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Search $data
+     * @param \AnimeDb\Bundle\CatalogBundle\Entity\Search $entity
      * @param integer $limit
      * @param integer $offset
      * @param string $sort_column
@@ -62,106 +68,33 @@ class SqlLike implements DriverInterface
      *
      * @return array {list:[],total:0}
      */
-    public function search(Search $data, $limit, $offset, $sort_column, $sort_direction)
+    public function search(Search $entity, $limit, $offset, $sort_column, $sort_direction)
     {
-        /* @var $selector \Doctrine\ORM\QueryBuilder */
-        $selector = $this->repository->createQueryBuilder('i');
-
-        // main name
-        if ($data->getName()) {
-            $selector
-                ->innerJoin('i.names', 'n')
-                ->andWhere('LOWER(i.name) LIKE :name OR LOWER(n.name) LIKE :name')
-                ->setParameter('name', preg_replace('/%+/', '%%', mb_strtolower($data->getName(), 'UTF8')).'%');
-        }
-        // date add
-        if ($data->getDateAdd() instanceof \DateTime) {
-            $selector->andWhere('i.date_add >= :date_add')
-                ->setParameter('date_add', $data->getDateAdd()->format('Y-m-d'));
-        }
-        // date premiere
-        if ($data->getDatePremiere() instanceof \DateTime) {
-            $selector->andWhere('i.date_premiere >= :date_premiere')
-                ->setParameter('date_premiere', $data->getDatePremiere()->format('Y-m-d'));
-        }
-        // date end
-        if ($data->getDateEnd() instanceof \DateTime) {
-            $selector->andWhere('i.date_end <= :date_end')
-                ->setParameter('date_end', $data->getDateEnd()->format('Y-m-d'));
-        }
-        // country
-        if ($data->getCountry() instanceof CountryEntity) {
-            $selector->andWhere('i.country = :country')
-                ->setParameter('country', $data->getCountry()->getId());
-        }
-        // storage
-        if ($data->getStorage() instanceof StorageEntity) {
-            $selector->andWhere('i.storage = :storage')
-                ->setParameter('storage', $data->getStorage()->getId());
-        }
-        // type
-        if ($data->getType() instanceof TypeEntity) {
-            $selector->andWhere('i.type = :type')
-                ->setParameter('type', $data->getType()->getId());
-        }
-        // genres
-        if ($data->getGenres()->count()) {
-            $ids = [];
-            foreach ($data->getGenres() as $key => $genre) {
-                $ids[] = (int)$genre->getId();
-            }
-            $selector->innerJoin('i.genres', 'g')
-                ->andWhere('g.id IN ('.implode(',', $ids).')');
-        }
-        // labels
-        if ($data->getLabels()->count()) {
-            $ids = [];
-            foreach ($data->getLabels() as $key => $label) {
-                $ids[] = (int)$label->getId();
-            }
-            $selector->innerJoin('i.labels', 'l')
-                ->andWhere('l.id IN ('.implode(',', $ids).')');
-        }
-        // studio
-        if ($data->getStudio() instanceof StudioEntity) {
-            $selector->andWhere('i.studio = :studio')
-                ->setParameter('studio', $data->getStudio()->getId());
-        }
-
-        // get count all items
-        $total = clone $selector;
-        $total = $total
-            ->select('COUNT(DISTINCT i)')
-            ->getQuery()
-            ->getSingleScalarResult();
-
-        // genres
-        if ($data->getGenres()->count()) {
-            $selector->andHaving('COUNT(i.id) = '.$data->getGenres()->count());
-        }
-
-        // apply order
-        $selector->orderBy('i.'.$sort_column, $sort_direction);
-        if ($sort_column != 'name') {
-            $selector->addOrderBy('i.name', $sort_direction);
-        }
-
-        if ($offset) {
-            $selector->setFirstResult($offset);
-        }
-        if ($limit) {
-            $selector->setMaxResults($limit);
-        }
-
-        // get items
-        $list = $selector
-            ->groupBy('i.id')
-            ->getQuery()
-            ->getResult();
+        $selector = $this->selector
+            ->create()
+            ->addCountry($entity)
+            ->addDateAdd($entity)
+            ->addDateEnd($entity)
+            ->addDatePremiere($entity)
+            ->addGenres($entity)
+            ->addLabels($entity)
+            ->addName($entity)
+            ->addStorage($entity)
+            ->addStudio($entity)
+            ->addType($entity)
+            ->sort($sort_column, $sort_direction)
+            ->limit($limit)
+            ->offset($offset);
 
         return [
-            'list' => $list,
-            'total' => $total
+            'list' => $selector
+                ->getQuerySelect()
+                ->getQuery()
+                ->getResult(),
+            'total' => $selector
+                ->getQueryTotal()
+                ->getQuery()
+                ->getSingleScalarResult()
         ];
     }
 
