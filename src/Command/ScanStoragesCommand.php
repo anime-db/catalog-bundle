b12454af8e09e169caba3c6804e50763efe59ca4
Peter Gribanov <info@peter-gribanov.ru>
2014-12-11 17:57:22 +0300

    correct compare file names

--- src/Command/ScanStoragesCommand.php
+++ src/Command/ScanStoragesCommand.php
@@ -263,17 +263,9 @@ EOT
      */
     protected function getItemFromFile(Storage $storage, SplFileInfo $file)
     {
-        // remove win:// if need
-        if (defined('PHP_WINDOWS_VERSION_BUILD')) {
-            $path = substr($file->getPathname(), 6);
-        } else {
-            $path = $file->getPathname();
-        }
-        $path .= $file->isDir() ? DIRECTORY_SEPARATOR : '';
-
         /* @var $item \AnimeDb\Bundle\CatalogBundle\Entity\Item */
         foreach ($storage->getItems() as $item) {
-            if ($item->getPath() == $path) {
+            if (pathinfo($item->getPath(), PATHINFO_BASENAME) == $file->getFilename()) {
                 return $item;
             }
         }
