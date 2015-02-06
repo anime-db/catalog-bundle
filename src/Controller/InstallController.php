e1f760a513d600e0c8430ea92a797e29cee6f254
Peter Gribanov <info@peter-gribanov.ru>
2015-02-06 14:37:07 +0300

    redirect to installation of app is not installed

---
+++ src/Controller/InstallController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Installation controller
+ *
+ * @package AnimeDb\Bundle\CatalogBundle\Controller
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class InstallController extends Controller
+{
+    /**
+     * Home
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $response = $this->get('cache_time_keeper')->getResponse();
+        // response was not modified for this request
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
+        return $this->render('AnimeDbCatalogBundle:Install:index.html.twig', [], $response);
+    }
+}
---
+++ src/Event/Listener/Request.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Event\Listener;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+
+/**
+ * Request listener
+ *
+ * @package AnimeDb\Bundle\AppBundle\Event\Listener
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Request
+{
+    /**
+     * Application is installed
+     *
+     * @var boolean
+     */
+    protected $installed = false;
+
+    /**
+     * Router
+     *
+     * @var \Symfony\Bundle\FrameworkBundle\Routing\Router
+     */
+    protected $router;
+
+    /**
+     * Construct
+     *
+     * @param boolean $installed
+     * @param \Symfony\Bundle\FrameworkBundle\Routing\Router $router
+     */
+    public function __construct($installed, Router $router) {
+        $this->installed = $installed;
+        $this->router = $router;
+    }
+
+    /**
+     * Kernel request handler
+     *
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if ($this->installed || $event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
+            return;
+        }
+
+        $url = $this->router->generate('install');
+        if ($url != $event->getRequest()->getRequestUri()) {
+            $event->setResponse(new RedirectResponse($url));
+        }
+    }
+}
