3bf2ae0644b4ec50e735e4fc1f44c6cc01afc127
Peter Gribanov <info@peter-gribanov.ru>
2015-02-06 17:35:14 +0300

    add form for change locale

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -30,12 +30,30 @@ class InstallController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // app already installed
+        if ($this->container->getParameter('anime_db.catalog.installed')) {
+            return $this->redirect($this->generateUrl('home'));
+        }
+
         $response = $this->get('cache_time_keeper')->getResponse();
         // response was not modified for this request
         if ($response->isNotModified($request)) {
             return $response;
         }
+        $form = $this->createForm('anime_db_catalog_install_locale')
+            ->handleRequest($request);
 
-        return $this->render('AnimeDbCatalogBundle:Install:index.html.twig', [], $response);
+        if ($form->isValid()) {
+            // update params
+            $this->get('anime_db.manipulator.parameters')
+                ->set('locale', $form->getData()['locale']);
+            // clear cache
+            $this->get('anime_db.cache_clearer')->clear();
+            // TODO redirect to step 2
+        }
+
+        return $this->render('AnimeDbCatalogBundle:Install:index.html.twig', [
+            'form' => $form->createView(),
+        ], $response);
     }
 }
