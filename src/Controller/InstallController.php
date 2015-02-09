a57bb1c9a1b3df7a03e3b54216ef0b1b6b4717f0
Peter Gribanov <info@peter-gribanov.ru>
2015-02-09 14:19:50 +0300

    add step 2 - add storage

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -12,6 +12,8 @@ namespace AnimeDb\Bundle\CatalogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AnimeDb\Bundle\CatalogBundle\Entity\Storage;
+use AnimeDb\Bundle\CatalogBundle\Form\Type\Entity\Storage as StorageForm;
 
 /**
  * Installation controller
@@ -49,10 +51,47 @@ class InstallController extends Controller
                 ->set('locale', $form->getData()['locale']);
             // clear cache
             $this->get('anime_db.cache_clearer')->clear();
-            // TODO redirect to step 2
+            // redirect to step 2
+            return $this->redirect($this->generateUrl('install_add_storage'));
         }
 
         return $this->render('AnimeDbCatalogBundle:Install:index.html.twig', [
+            'form' => $form->createView(),
+        ], $response);
+    }
+
+    /**
+     * Add storage
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function addStorageAction(Request $request)
+    {
+        // app already installed
+        if ($this->container->getParameter('anime_db.catalog.installed')) {
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+        $response = $this->get('cache_time_keeper')->getResponse();
+        // response was not modified for this request
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+        $storage = new Storage();
+        /* @var $form \Symfony\Component\Form\Form */
+        $form = $this->createForm(new StorageForm(), $storage)
+            ->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($storage);
+            $em->flush();
+            // TODO redirect to step 3
+        }
+
+        return $this->render('AnimeDbCatalogBundle:Install:add_storage.html.twig', [
             'form' => $form->createView(),
         ], $response);
     }
