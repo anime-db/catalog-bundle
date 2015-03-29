3aee0a2159eb4bbe45932650f481ea7d9ce88d04
Peter Gribanov <info@peter-gribanov.ru>
2015-03-29 20:16:24 +0300

    add default search form field

--- src/Form/Type/Install/Settings.php
+++ src/Form/Type/Install/Settings.php
@@ -13,6 +13,7 @@ namespace AnimeDb\Bundle\CatalogBundle\Form\Type\Install;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Search\Chain;
 
 /**
  * Settings for installation page
@@ -28,6 +29,23 @@ class Settings extends AbstractType
      * @var \Symfony\Component\HttpFoundation\Request|null
      */
     protected $request;
+
+    /**
+     * Search chain
+     *
+     * @var \AnimeDb\Bundle\CatalogBundle\Plugin\Search\Filler\Chain
+     */
+    protected $chain;
+
+    /**
+     * Construct
+     *
+     * @param \AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Search\Chain $cain
+     */
+    public function __construct(Chain $cain)
+    {
+        $this->chain = $cain;
+    }
 
     /**
      * Set request
@@ -47,10 +65,21 @@ class Settings extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $search_choices = ['' => 'No'];
+        /* @var $plugin \AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Search\Search */
+        foreach ($this->chain->getPlugins() as $plugin) {
+            $search_choices[$plugin->getName()] = $plugin->getTitle();
+        }
+
         $builder
             ->add('locale', 'locale', [
                 'label' => 'Language',
                 'data' => $this->request ? $this->request->getLocale() : ''
+            ])
+            ->add('default_search', 'choice', [
+                'required' => false,
+                'choices' => $search_choices,
+                'label' => 'Default search plugin'
             ]);
     }
 
