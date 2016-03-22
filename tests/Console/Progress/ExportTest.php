81189ef795ab5d337ac687dc5d9aded30cdab969
Peter Gribanov <wcode404@gmail.com>
2016-03-22 12:54:09 +0300

    change chmod for test files

--- tests/Console/Progress/ExportTest.php
+++ tests/Console/Progress/ExportTest.php
@@ -13,6 +13,7 @@ namespace AnimeDb\Bundle\CatalogBundle\Tests\Console\Progress;
 use AnimeDb\Bundle\CatalogBundle\Console\Progress\Export;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Helper\ProgressHelper;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Test export progress
@@ -36,7 +37,9 @@ class ExportTest extends \PHPUnit_Framework_TestCase
 
     protected function tearDown()
     {
-        (new Filesystem())->remove($this->root);
+        $fs = new Filesystem();
+        $fs->chmod([$this->root, $this->root.'example.log'], 0777);
+        $fs->remove($this->root);
     }
 
     /**
@@ -45,11 +48,13 @@ class ExportTest extends \PHPUnit_Framework_TestCase
     public function testExport()
     {
         $filename = $this->root.'example.log';
+        /* @var $progress \PHPUnit_Framework_MockObject_MockObject|ProgressHelper */
         $progress = $this->getMock('\Symfony\Component\Console\Helper\ProgressHelper');
         $progress
             ->expects($this->once())
             ->method('setFormat')
             ->with(ProgressHelper::FORMAT_QUIET);
+        /* @var $output \PHPUnit_Framework_MockObject_MockObject|OutputInterface */
         $output = $this->getMock('\Symfony\Component\Console\Output\OutputInterface');
         $output
             ->expects($this->at(0))
