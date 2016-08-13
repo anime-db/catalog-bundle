28516bfe1be1ddb74b4d3cdeffa3b65a07d42a5b
Peter Gribanov <info@peter-gribanov.ru>
2016-08-13 14:40:18 +0300

    use menu builder as a service #225 fixed

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -8,24 +8,28 @@
  */
 namespace AnimeDb\Bundle\CatalogBundle\Menu;
 
-use AnimeDb\Bundle\CatalogBundle\Plugin\PluginInMenuInterface;
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use AnimeDb\Bundle\ApiClientBundle\Service\Client;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Chain;
 use AnimeDb\Bundle\CatalogBundle\Entity\Item;
+use AnimeDb\Bundle\CatalogBundle\Plugin\PluginInMenuInterface;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Import\Chain as ChainImport;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Export\Chain as ChainExport;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Filler\Chain as ChainFiller;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Search\Chain as ChainSearch;
+use AnimeDb\Bundle\CatalogBundle\Plugin\Setting\Chain as ChainSetting;
+use AnimeDb\Bundle\CatalogBundle\Plugin\Item\Chain as ChainItem;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Item\ItemInterface as ItemPluginInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Menu builder.
  *
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-class Builder extends ContainerAware
+class Builde
 {
     /**
      * Link to guide by update the application on Windows XP.
@@ -35,104 +39,208 @@ class Builder extends ContainerAware
     const GUIDE_LINK = '/guide/';
 
     /**
+     * @var FactoryInterface
+     */
+    protected $factory;
+
+    /**
+     * @var RequestStack
+     */
+    protected $request_stack;
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var Client
+     */
+    protected $api_client;
+
+    /**
+     * @var ChainImport
+     */
+    protected $plugin_import;
+
+    /**
+     * @var ChainExport
+     */
+    protected $plugin_export;
+
+    /**
+     * @var ChainSearch
+     */
+    protected $plugin_search;
+
+    /**
+     * @var ChainFiller
+     */
+    protected $plugin_filler;
+
+    /**
+     * @var ChainSetting
+     */
+    protected $plugin_setting;
+
+    /**
+     * @var ChainItem
+     */
+    protected $plugin_item;
+
+    /**
      * @param FactoryInterface $factory
-     * @param array $options
-     *
+     * @param RequestStack $request_stack
+     * @param TranslatorInterface $translator
+     * @param Client $api_client
+     * @param ChainImport $plugin_import
+     * @param ChainExport $plugin_export
+     * @param ChainSearch $plugin_search
+     * @param ChainFiller $plugin_filler
+     * @param ChainSetting $plugin_setting
+     * @param ChainItem $plugin_item
+     */
+    public function __construct(
+        FactoryInterface $factory,
+        RequestStack $request_stack,
+        TranslatorInterface $translator,
+        Client $api_client,
+        ChainImport $plugin_import,
+        ChainExport $plugin_export,
+        ChainSearch $plugin_search,
+        ChainFiller $plugin_filler,
+        ChainSetting $plugin_setting,
+        ChainItem $plugin_item
+    ) {
+        $this->factory = $factory;
+        $this->request_stack = $request_stack;
+        $this->translator = $translator;
+        $this->api_client = $api_client;
+        $this->plugin_import = $plugin_import;
+        $this->plugin_export = $plugin_export;
+        $this->plugin_search = $plugin_search;
+        $this->plugin_filler = $plugin_filler;
+        $this->plugin_setting = $plugin_setting;
+        $this->plugin_item = $plugin_item;
+    }
+
+    /**
      * @return ItemInterface
      */
-    public function mainMenu(FactoryInterface $factory, array $options)
+    public function createMainMenu()
     {
         /* @var $menu ItemInterface */
-        $menu = $factory->createItem('root');
+        $menu = $this->factory
+            ->createItem('root')
+            ->setUri($this->request_stack->getMasterRequest()->getRequestUri());
 
-        $menu->addChild('Search', ['route' => 'home_search'])
+        $menu
+            ->addChild('Search', ['route' => 'home_search'])
             ->setLinkAttribute('class', 'icon-label icon-gray-search');
-        $add = $menu->addChild('Add record')
+        $add = $menu
+            ->addChild('Add record')
             ->setLabelAttribute('class', 'icon-label icon-gray-add');
 
         // synchronization items
-        /* @var $import ChainImport */
-        $import = $this->container->get('anime_db.plugin.import');
-        /* @var $export ChainExport */
-        $export = $this->container->get('anime_db.plugin.export');
-        if ($import->hasPlugins() || $export->hasPlugins()) {
-            $sync = $menu->addChild('Synchronization')
+        if ($this->plugin_import->hasPlugins() || $this->plugin_export->hasPlugins()) {
+            $sync = $menu
+                ->addChild('Synchronization')
                 ->setLabelAttribute('class', 'icon-label icon-white-cloud-sync');
+
             // add import plugin items
-            $this->addPluginItems($import, $sync, 'Import items', '', 'icon-label icon-white-cloud-download');
+            $this->addPluginItems(
+                $this->plugin_import,
+                $sync,
+                'Import items',
+                '',
+                'icon-label icon-white-cloud-download'
+            );
             // add export plugin items
-            $this->addPluginItems($export, $sync, 'Export items', '', 'icon-label icon-white-cloud-arrow-up');
+            $this->addPluginItems(
+                $this->plugin_export,
+                $sync,
+                'Export items',
+                '',
+                'icon-label icon-white-cloud-arrow-up'
+            );
         }
 
-        $settings = $menu->addChild('Settings')
+        $settings = $menu
+            ->addChild('Settings')
             ->setLabelAttribute('class', 'icon-label icon-gray-settings');
 
         // add search plugin items
-        /* @var $chain ChainSearch */
-        $chain = $this->container->get('anime_db.plugin.search_fill');
         $this->addPluginItems(
-            $chain,
+            $this->plugin_search,
             $add,
             'Search by name',
             'Search by name the source of filling item',
             'icon-label icon-white-search'
         );
-        if ($chain->hasPlugins()) {
-            $add->addChild('Search in all plugins', ['route' => 'fill_search_in_all'])
-                ->setAttribute('title', $this->container->get('translator')->trans('Search by name in all plugins'))
+        if ($this->plugin_search->hasPlugins()) {
+            $add
+                ->addChild('Search in all plugins', ['route' => 'fill_search_in_all'])
+                ->setAttribute('title', $this->translator->trans('Search by name in all plugins'))
                 ->setLinkAttribute('class', 'icon-label icon-white-cloud-search');
-            $add->addChild('Add from URL', ['route' => 'fill_search_filler'])
-                ->setAttribute('title', $this->container->get('translator')->trans('Search plugin by the URL for filling item'))
+            $add
+                ->addChild('Add from URL', ['route' => 'fill_search_filler'])
+                ->setAttribute('title', $this->translator->trans('Search plugin by the URL for filling item'))
                 ->setLinkAttribute('class', 'icon-label icon-white-cloud-search');
         }
         // add filler plugin items
-        /* @var $filler ChainFiller */
-        $filler = $this->container->get('anime_db.plugin.filler');
         $this->addPluginItems(
-            $filler,
+            $this->plugin_filler,
             $add,
             'Fill from source',
             'Fill record from source (example source is URL)',
             'icon-label icon-white-share'
         );
         // add manually
-        $add->addChild('Fill manually', ['route' => 'item_add_manually'])
+        $add
+            ->addChild('Fill manually', ['route' => 'item_add_manually'])
             ->setLinkAttribute('class', 'icon-label icon-white-add');
 
-        $settings->addChild('File storages', ['route' => 'storage_list'])
+        $settings
+            ->addChild('File storages', ['route' => 'storage_list'])
             ->setLinkAttribute('class', 'icon-label icon-white-storage');
-        $settings->addChild('List of notice', ['route' => 'notice_list'])
+        $settings
+            ->addChild('List of notice', ['route' => 'notice_list'])
             ->setLinkAttribute('class', 'icon-label icon-white-alert');
-        $settings->addChild('Labels', ['route' => 'label'])
+        $settings
+            ->addChild('Labels', ['route' => 'label'])
             ->setLinkAttribute('class', 'icon-label icon-white-label');
-        $plugins = $settings->addChild('Plugins')
+        $plugins = $settings
+            ->addChild('Plugins')
             ->setLabelAttribute('class', 'icon-label icon-white-plugin');
-        $settings->addChild('Update', ['route' => 'update'])
+        $settings
+            ->addChild('Update', ['route' => 'update'])
             ->setLinkAttribute('class', 'icon-label icon-white-update');
-        $settings->addChild('General', ['route' => 'home_settings'])
+        $settings
+            ->addChild('General', ['route' => 'home_settings'])
             ->setLinkAttribute('class', 'icon-label icon-white-settings');
 
         // plugins
-        $plugins->addChild('Installed', ['route' => 'plugin_installed'])
+        $plugins
+            ->addChild('Installed', ['route' => 'plugin_installed'])
             ->setLinkAttribute('class', 'icon-label icon-white-plugin');
-        $plugins->addChild('Store', ['route' => 'plugin_store'])
+        $plugins
+            ->addChild('Store', ['route' => 'plugin_store'])
             ->setLinkAttribute('class', 'icon-label icon-white-shop');
         // add settings plugin items
-        foreach ($this->container->get('anime_db.plugin.setting')->getPlugins() as $plugin) {
+        foreach ($this->plugin_setting->getPlugins() as $plugin) {
             /* @var $plugin PluginInMenuInterface */
             $plugin->buildMenu($plugins);
         }
 
         // add link to guide
-        $settings->addChild('Help', ['uri' => $this->container->get('anime_db.api.client')->getSiteUrl(self::GUIDE_LINK)])
+        $settings
+            ->addChild('Help', ['uri' => $this->api_client->getSiteUrl(self::GUIDE_LINK)])
             ->setLinkAttribute('class', 'icon-label icon-white-help');
 
         return $menu;
     }
 
     /**
-     * Add plugin items in menu.
-     *
      * @param Chain $chain
      * @param ItemInterface $root
      * @param string $label
@@ -144,7 +252,7 @@ class Builder extends ContainerAware
         if ($chain->hasPlugins()) {
             $group = $root->addChild($label);
             if ($title) {
-                $group->setAttribute('title', $this->container->get('translator')->trans($title));
+                $group->setAttribute('title', $this->translator->trans($title));
             }
             if ($class) {
                 $group->setLabelAttribute('class', $class);
@@ -152,46 +260,49 @@ class Builder extends ContainerAware
 
             // add child items
             foreach ($chain->getPlugins() as $plugin) {
-                /* @var $plugin PluginInMenuInterface */
-                $plugin->buildMenu($group);
+                if ($plugin instanceof PluginInMenuInterface) {
+                    $plugin->buildMenu($group);
+                }
             }
         }
     }
 
     /**
-     * @param FactoryInterface $factory
      * @param array $options
      *
      * @return ItemInterface
      */
-    public function itemMenu(FactoryInterface $factory, array $options)
+    public function createItemMenu(array $options)
     {
         if (empty($options['item']) || !($options['item'] instanceof Item)) {
             throw new \InvalidArgumentException('Item is not found');
         }
+
         /* @var $item Item */
         $item = $options['item'];
 
         /* @var $menu ItemInterface */
-        $menu = $factory->createItem('root');
+        $menu = $this->factory
+            ->createItem('root')
+            ->setUri($this->request_stack->getMasterRequest()->getRequestUri());
         $params = [
             'id' => $item->getId(),
             'name' => $item->getUrlName(),
         ];
 
-        $menu->addChild('Change record', ['route' => 'item_change', 'routeParameters' => $params])
+        $menu
+            ->addChild('Change record', ['route' => 'item_change', 'routeParameters' => $params])
             ->setLinkAttribute('class', 'icon-label icon-edit');
 
         // add settings plugin items
-        $chain = $this->container->get('anime_db.plugin.item');
         /* @var $plugin ItemPluginInterface */
-        foreach ($chain->getPlugins() as $plugin) {
+        foreach ($this->plugin_item->getPlugins() as $plugin) {
             $plugin->buildMenu($menu, $options['item']);
         }
 
         $menu->addChild('Delete record', ['route' => 'item_delete', 'routeParameters' => $params])
             ->setLinkAttribute('class', 'icon-label icon-delete')
-            ->setLinkAttribute('data-message', $this->container->get('translator')->trans(
+            ->setLinkAttribute('data-message', $this->translator->trans(
                 'Are you sure want to delete %name%?',
                 ['%name%' => $item->getName()]
             ));
