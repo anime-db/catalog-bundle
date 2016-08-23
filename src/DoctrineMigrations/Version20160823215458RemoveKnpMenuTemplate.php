dc7ff5ec0ed60e629795f4b2c98f446d774d329b
Peter Gribanov <info@peter-gribanov.ru>
2016-08-23 22:46:14 +0300

    correct migration namespace

--- src/DoctrineMigrations/Version20160823215458RemoveKnpMenuTemplate.php
+++ src/DoctrineMigrations/Version20160823215458RemoveKnpMenuTemplate.php
@@ -1,6 +1,12 @@
 <?php
-
-namespace Application\Migrations;
+/**
+ * AnimeDb package.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+namespace AnimeDb\Bundle\CatalogBundle\DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
