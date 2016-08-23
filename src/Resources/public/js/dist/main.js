637f60c767e302ee5149cd7bd08aed0ddd63025a
Peter Gribanov <info@peter-gribanov.ru>
2016-08-23 22:36:44 +0300

    correct indent

--- src/Resources/public/js/dist/main.js
+++ src/Resources/public/js/dist/main.js
@@ -1419,13 +1419,13 @@ $.fn.clearFields = $.fn.clearInputs = function(includeHidden) {
         else if (tag == 'select') {
             this.selectedIndex = -1;
         }
-		else if (t == "file") {
-			if (/MSIE/.test(navigator.userAgent)) {
-				$(this).replaceWith($(this).clone(true));
-			} else {
-				$(this).val('');
-			}
-		}
+        else if (t == "file") {
+            if (/MSIE/.test(navigator.userAgent)) {
+                $(this).replaceWith($(this).clone(true));
+            } else {
+                $(this).val('');
+            }
+        }
         else if (includeHidden) {
             // includeHidden can be the value true, or it can be a selector string
             // indicating a special test; for example:
@@ -2799,51 +2799,51 @@ var FormContainer = new BlockLoadHandler();
 
 // registr form collection
 FormContainer.registr(function(block) {
-	block.find('.f-collection > div').each(function() {
-		var collection = $(this);
-		CollectionContainer.add(
-			new FormCollection(
-				collection,
-				collection.find('.bt-add-item'),
-				collection.find('.f-row:not(.f-coll-button)'),
-				'.bt-remove-item',
-				FormContainer
-			)
-		);
-	});
+    block.find('.f-collection > div').each(function() {
+        var collection = $(this);
+        CollectionContainer.add(
+            new FormCollection(
+                collection,
+                collection.find('.bt-add-item'),
+                collection.find('.f-row:not(.f-coll-button)'),
+                '.bt-remove-item',
+                FormContainer
+            )
+        );
+    });
 });
 
 // registr form image
 FormContainer.registr(function(block) {
-	block.find('.f-image').each(function() {
-		new FormImageController($(this));
-	});
+    block.find('.f-image').each(function() {
+        new FormImageController($(this));
+    });
 });
 // registr form local path
 FormContainer.registr(function(block) {
-	block.find('.f-local-path').each(function() {
-		new FormLocalPathController($(this));
-	});
+    block.find('.f-local-path').each(function() {
+        new FormLocalPathController($(this));
+    });
 });
 
 // registr form check all
 FormContainer.registr(function(block) {
-	new TableCheckAllController(block.find('.f-table-check-all'));
+    new TableCheckAllController(block.find('.f-table-check-all'));
 });
 
 // registr form text autocomplete
 FormContainer.registr(function(block) {
-	block.find('input[type=search]:not([data-source=""])').each(function() {
-		var input = $(this);
-		var length = 2;
-		if (typeof input.data('min-length') !== 'undefined') {
-			length = input.data('min-length');
-		}
-		input.autocomplete({
-			source: input.data('source'),
-			minLength: length
-		});
-	});
+    block.find('input[type=search]:not([data-source=""])').each(function() {
+        var input = $(this);
+        var length = 2;
+        if (typeof input.data('min-length') !== 'undefined') {
+            length = input.data('min-length');
+        }
+        input.autocomplete({
+            source: input.data('source'),
+            minLength: length
+        });
+    });
 });
 
 
@@ -2854,116 +2854,116 @@ FormContainer.notify($(document));
 // init notice container
 var container = $('#notice-container');
 if (container.size() && (from = container.data('from'))) {
-	new NoticeContainerModel(container, from);
+    new NoticeContainerModel(container, from);
 }
 
 // confirm delete
 $('.icon-delete, .b-notice-table button[type=submit]').each(function() {
-	var ConfirmDeleteNotice = ConfirmDeleteModel;
-	ConfirmDeleteNotice.prototype.action = null;
-	ConfirmDeleteNotice.prototype.getAction = function() {
-		if (!this.action) {
-			this.action = $(this.link.data('target'));
-		}
-		return this.action.val();
-	};
-	ConfirmDeleteNotice.prototype.remove = function() {
-		if (this.getAction() == this.link.data('action')) {
-			return confirm(this.massage);
-		}
-		return true;
-	};
-	new ConfirmDeleteNotice($(this));
+    var ConfirmDeleteNotice = ConfirmDeleteModel;
+    ConfirmDeleteNotice.prototype.action = null;
+    ConfirmDeleteNotice.prototype.getAction = function() {
+        if (!this.action) {
+            this.action = $(this.link.data('target'));
+        }
+        return this.action.val();
+    };
+    ConfirmDeleteNotice.prototype.remove = function() {
+        if (this.getAction() == this.link.data('action')) {
+            return confirm(this.massage);
+        }
+        return true;
+    };
+    new ConfirmDeleteNotice($(this));
 });
 
 $('.b-update-container code').each(function() {
-	new UpdateLogBlock($(this));
+    new UpdateLogBlock($(this));
 });
 
 // registr form storage
 $('.f-storage').each(function() {
-	var storage = $(this);
-	new FormStorage(storage, storage.data('source'), $(storage.data('target')));
+    var storage = $(this);
+    new FormStorage(storage, storage.data('source'), $(storage.data('target')));
 });
 
 // init form field refill
 var refills = $('[data-type=refill]');
 var form = refills.closest('form');
 refills.each(function() {
-	var field = $(this);
-	var controller;
-	if (field.data('prototype')) {
-		controller = new FormRefillCollection(
-			field,
-			CollectionContainer.get(field.attr('id')),
-			CollectionContainer
-		);
-	} else {
-		if (field.find('input').size() > 1) {
-			controller = new FormRefillMulti(field);
-		} else {
-			controller = new FormRefillSimple(field);
-		}
-	}
+    var field = $(this);
+    var controller;
+    if (field.data('prototype')) {
+        controller = new FormRefillCollection(
+            field,
+            CollectionContainer.get(field.attr('id')),
+            CollectionContainer
+        );
+    } else {
+        if (field.find('input').size() > 1) {
+            controller = new FormRefillMulti(field);
+        } else {
+            controller = new FormRefillSimple(field);
+        }
+    }
 
-	// add plugin links and hendler
-	$(field.closest('.f-row').find('label')[0])
-		.append($(field.data('plugins')))
-		.find('a').each(function() {
-			new FormRefill(
-				form,
-				$(this),
-				controller,
-				FormContainer,
-				CollectionContainer.get('anime_db_catalog_entity_item_sources')
-			);
-		});
+    // add plugin links and hendler
+    $(field.closest('.f-row').find('label')[0])
+        .append($(field.data('plugins')))
+        .find('a').each(function() {
+            new FormRefill(
+                form,
+                $(this),
+                controller,
+                FormContainer,
+                CollectionContainer.get('anime_db_catalog_entity_item_sources')
+            );
+        });
 });
 
 if (jQuery().fancybox) {
-	$('[data-control="gallery"]').fancybox({
-		titlePosition: 'over',
-		openEffect: 'fade',
-		closeEffect: 'fade',
-		helpers: {
-			title: null
-		}
-	});
+    $('[data-control="gallery"]').fancybox({
+        titlePosition: 'over',
+        openEffect: 'fade',
+        closeEffect: 'fade',
+        helpers: {
+            title: null
+        }
+    });
 }
 
 $('.bt-toggle-block').each(function() {
-	new ToggleBlock($(this));
+    new ToggleBlock($(this));
 
-	var block = $(this);
-	var icon = block.find('.bt-toggle-block-icon');
-	if (icon.size()) {
-		block.click(function() {
-			if (icon.text() == '▾') {
-				icon.text('▴');
-			} else {
-				icon.text('▾');
-			}
-		});
-	}
+    var block = $(this);
+    var icon = block.find('.bt-toggle-block-icon');
+    if (icon.size()) {
+        block.click(function() {
+            if (icon.text() == '▾') {
+                icon.text('▴');
+            } else {
+                icon.text('▾');
+            }
+        });
+    }
 });
 
 $('.list-items .item-container').each(function() {
-	new KeepHover($(this));
+    new KeepHover($(this));
 });
 
 $('.b-progress .b-progress-bar[data-from]').each(function() {
-	var bar = $(this);
-	new ProgressBar(bar, bar.find('.b-progress-label'));
+    var bar = $(this);
+    new ProgressBar(bar, bar.find('.b-progress-label'));
 });
 
 $('.b-progress:not(.b-update-container) .b-progress-log code[data-from]').each(function() {
-	var code = $(this);
-	new ProgressLog(code, code.closest('pre'));
+    var code = $(this);
+    new ProgressLog(code, code.closest('pre'));
 });
 
 $('form').submit(function(){
-	$(this).find('input[type=submit], button[type=submit]').attr('disabled', 'disabled');
-	$('body').addClass('progress');
+    $(this).find('input[type=submit], button[type=submit]').attr('disabled', 'disabled');
+    $('body').addClass('progress');
 });
 
 });
